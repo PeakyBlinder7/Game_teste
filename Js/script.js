db0230115ef8a83e5bc4a384eac359126c32b58b
Caio Carvalho <two_Caio@live.com>
2022-07-14 23:38:19 -0300

    novas mudanças no game

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,46 @@
-const boneco = document.querySelector('.boneco');
-const corvo = document.querySelector('corvo');
+const boneco = document.querySelector('#mc');
+const corvo = document.querySelector('#raven');
+const btn = document.querySelector('#btn');
+const menu = document.querySelector('#start-menu');
+let CanJump = true;
 
-const jump = () => {
-  boneco.classList.add('jump');
+function Sumiu() 
+{
+  setTimeout(menu.classList.add('esta-invisivel'), 300);
 
-  setTimeout(() => {
-
-    boneco.classList.remove('jump');
-
-
-  }, 400);
-  
+  CorvoAndando();
 }
 
-document.addEventListener('keydown',jump);
+function CorvoAndando()
+{
+  corvo.classList.add('corvo-que-anda');
+}
+
+
+function RemoverPulo() 
+{
+  boneco.classList.remove('jump');
+  CanJump = true;
+}
+
+
+function AdicionarPulo() 
+{
+  CanJump = false;
+  boneco.classList.add('jump');
+  //depois de adicionar a classe CSS jump, vai esperar 500 milisegundos para remover (tempo de execução);
+  setTimeout(RemoverPulo, 600);
+}
+
+function WillJump() {
+  if (CanJump == true)
+  {
+    AdicionarPulo();
+  }
+}
+
+
+
+//game loop
+document.addEventListener('keypress', WillJump);
+btn.addEventListener('click', () => Sumiu());
