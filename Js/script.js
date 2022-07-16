22adea41932004c867c6b76c37fa835fec8d39df
Caio Carvalho <two_Caio@live.com>
2022-07-15 22:16:05 -0300

    game loop e colisao finalizados

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,48 @@ const boneco = document.querySelector('#mc');
 const corvo = document.querySelector('#raven');
 const btn = document.querySelector('#btn');
 const menu = document.querySelector('#start-menu');
+const pontuacao = document.querySelector('#score');
+const btnrestart = document.querySelector('#btnrestart');
+const gameoverscreen = document.querySelector('#gameover');
+const finalscore = document.querySelector('#finalscore');
 let CanJump = true;
+let pontos = 0;
+let VaiMorre = true;
+
+let intervalo = function (func)
+{
+  return setInterval(func, 10);
+}
+
+function DeathCheck()
+{
+  let McTop = parseInt(window.getComputedStyle(boneco).getPropertyValue("top"));
+  let CorvoLeft = parseInt(window.getComputedStyle(corvo).getPropertyValue("left"));
+
+  if(VaiMorre == true && CorvoLeft < 100 && CorvoLeft > 0 && McTop > 210)
+  {
+    gameoverscreen.classList.remove('esta-invisivel');
+    finalscore.innerHTML = `Pontuação final: ${pontos}`;
+    VaiMorre = false;
+  } else if(VaiMorre == true && CorvoLeft <= 1)
+  {
+    IncreaseScore();
+  }
+
+}
+
+function IncreaseScore()
+{
+    pontos++;
+
+    if(pontos <= 9)
+    {
+        pontuacao.innerHTML = "0" + pontos;
+    } else
+    {
+        pontuacao.innerHTML = pontos;
+    }
+}
 
 function Sumiu() 
 {
@@ -28,8 +69,8 @@ function AdicionarPulo()
 {
   CanJump = false;
   boneco.classList.add('jump');
-  //depois de adicionar a classe CSS jump, vai esperar 500 milisegundos para remover (tempo de execução);
-  setTimeout(RemoverPulo, 600);
+  //depois de adicionar a classe CSS jump, vai esperar 1300 milisegundos para remover (tempo de execução);
+  setTimeout(RemoverPulo, 1300);
 }
 
 function WillJump() {
@@ -44,3 +85,12 @@ function WillJump() {
 //game loop
 document.addEventListener('keypress', WillJump);
 btn.addEventListener('click', () => Sumiu());
+btnrestart.addEventListener('click', () => document.location.reload(true));
+intervalo(DeathCheck);
+
+
+
+
+
+
+
